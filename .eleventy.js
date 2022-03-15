cb084d7bc2637b6d7a6c51b0f6f9d41bd1d66d3e
Dennis Ostendorf <ostendorf@technologiestiftung-berlin.de>
2022-03-15 18:04:43 +0100

    chore(SAMS-40): add placeholder method PDF

--- .eleventy.js
+++ .eleventy.js
@@ -10,13 +10,16 @@ module.exports = function (eleventyConfig) {
   // Include all content in the fonts folder
   eleventyConfig.addPassthroughCopy("src/assets/fonts");
 
+  // Include all content in the docs folder
+  eleventyConfig.addPassthroughCopy("src/assets/docs");
+
   // Minify HTML
-  eleventyConfig.addTransform("htmlmin", function(content) {
-    if(this.outputPath && this.outputPath.endsWith(".html")) {
+  eleventyConfig.addTransform("htmlmin", function (content) {
+    if (this.outputPath && this.outputPath.endsWith(".html")) {
       return htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
-        collapseWhitespace: true
+        collapseWhitespace: true,
       });
     }
 
