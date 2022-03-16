7e79ca162b191a0931c648503e6bc5d3cd152298
Lucas Vogel <contact@vogelino.com>
2022-03-16 17:38:13 +0100

    refactor(footer): Remove unnecessary js code

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -1,21 +1,6 @@
 {
   const backButton = document.getElementById("back-to-top-link");
 
-  function updateBackToTopButton() {
-    if (window.scrollY > 400) {
-      backButton.classList.add("show");
-    } else {
-      backButton.classList.remove("show");
-    }
-    let to;
-    setTimeout(() => {
-      clearTimeout(to);
-      window.requestAnimationFrame(updateBackToTopButton);
-    })
-  }
-
-  window.requestAnimationFrame(updateBackToTopButton);
-
   backButton
     .addEventListener("click", function(e) {
       e.preventDefault();
