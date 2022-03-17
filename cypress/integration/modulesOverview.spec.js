07dc0c05e2a36cf18a8f6b09459e51d42a835df0
Lucas Vogel <contact@vogelino.com>
2022-03-17 14:51:53 +0100

    test(modules_overview): Add cypress tests to modules overview

--- cypress/integration/modulesOverview.spec.js
+++ cypress/integration/modulesOverview.spec.js
@@ -8,4 +8,23 @@ describe("Modules Overview", () => {
       name: /Die 8 Module/i,
     }).should("exist");
   });
+
+  it("renders the hero blocks", () => {
+    cy.findAllByRole("listitem", {
+      name: /Komponent des Moduls/i,
+    }).should("have.length", 4);
+  });
+
+  it("renders links to all modules", () => {
+    cy.findAllByRole("link", {
+      name: /Link zu den Modul .+/i,
+    }).should("have.length", 8);
+  });
+
+  it("renders images of to all modules", () => {
+    const main = cy.findByRole("list", {
+      name: /Liste aller Module/i,
+    });
+    main.findAllByRole("img").should("have.length", 8);
+  });
 });
