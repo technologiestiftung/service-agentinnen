833256a4b4c819f324c9af27d3eba9199a270de5
Lucas Vogel <contact@vogelino.com>
2022-03-29 11:10:19 +0200

    refactor(SAMS-79): Host video and remove youtube

--- src/assets/js/home.js
+++ src/assets/js/home.js
@@ -1,15 +1,13 @@
 {
-  document.getElementById('video-placeholder').addEventListener('click', function() {
-    const placeholder$ = document.getElementById('video-placeholder');
-    placeholder$.innerHTML = `
-      <iframe
-        class="absolute inset-0 w-full h-full aspect-video"
-        src="https://www.youtube-nocookie.com/embed/iqpsEzLdl_k?autoplay=1"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
+  document
+    .getElementById("video-placeholder")
+    .addEventListener("click", function () {
+      const placeholder$ = document.getElementById("video-placeholder");
+      placeholder$.innerHTML = `
+      <video class="absolute inset-0 w-full h-full aspect-video" controls autoplay>
+        <source src="/assets/videos/introduction.webm" type="video/webm" />
+        <source src="/assets/videos/introduction.mp4" type="video/mp4" />
+      </video>
     `;
-  });
+    });
 }
