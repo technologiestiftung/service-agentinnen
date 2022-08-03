507e3c589e7c2c8ce32f57332cfaf2c386662169
Lucas Vogel <contact@vogelino.com>
2022-08-03 17:06:00 +0200

    fix(matomo): Update code snippet

--- src/assets/js/matomo.js
+++ src/assets/js/matomo.js
@@ -1,9 +1,7 @@
-// MATOMO TRACKING
 var _paq = (window._paq = window._paq || []);
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(["trackPageView"]);
 _paq.push(["enableLinkTracking"]);
-_paq.push(["requireCookieConsent"]);
 (function () {
   var u = "https://piwik.technologiestiftung-berlin.de/";
   _paq.push(["setTrackerUrl", u + "matomo.php"]);
@@ -11,7 +9,6 @@ _paq.push(["requireCookieConsent"]);
   var d = document,
     g = d.createElement("script"),
     s = d.getElementsByTagName("script")[0];
-  g.type = "text/javascript";
   g.async = true;
   g.src = u + "matomo.js";
   s.parentNode.insertBefore(g, s);
